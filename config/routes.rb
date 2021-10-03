d603248112c32538d526bc074359bbb227d630f2
RyokoFJBYS <ryokofujibayashi828@gmail.com>
2021-10-03 14:47:43 +0000

    [Add]ユーザー新規登録/ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
  root to: 'homes#top'
  get "about" => "homes#about"
  resources :books
- 
+
  resources :users, only: [:show, :index, :edit, :update]
- 
+
 end
