d1bd7d021157d2f3b57c38b3ea9e537caa70e78f
RyokoFJBYS <ryokofujibayashi828@gmail.com>
2021-10-04 14:48:30 +0000

    [Add]CSS実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
   def show
     @book = Book.new
-    @book = Book.find(params[:id])
     @user = User.find(params[:id])
+    @books = @user.books
   end
-  
+
   def index
     @users = User.all
     @book = Book.new
     @user = current_user
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
 end
